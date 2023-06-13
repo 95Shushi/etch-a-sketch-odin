53b76ef6f021e6badf59a2cffdb336b3d81504d7
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-13 14:01:34 +0800

    Format JavaScript

--- script.js
+++ script.js
@@ -20,24 +20,24 @@ function changeGrid() {
     while (containerDiv.hasChildNodes()) {
         containerDiv.removeChild(containerDiv.firstChild);
     }
-    let gridNumber = prompt("Select a grid size n x n")
+    let gridNumber = prompt("Select a grid size n x n");
     if (gridNumber < 1 || gridNumber > 100 || isNaN(gridNumber)) {
-        alert("Please type a valid number")
+        alert("Please type a valid number");
     } else {
         let squareNumber = gridNumber * gridNumber;
-        let squareSize = ((672/gridNumber) - 2);
+        let squareSize = 672 / gridNumber - 2;
         for (let i = 1; i <= squareNumber; i++) {
             let div = document.createElement("div");
             div.classList.add("inside-div");
             div.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px`);
             containerDiv.appendChild(div);
-        };
+        }
         let grid = document.querySelectorAll(".inside-div");
         grid.forEach((square) => {
-        square.addEventListener("mouseover", changeColor);
-});
+            square.addEventListener("mouseover", changeColor);
+        });
     }
 }
 
-let button = document.querySelector("#change-grid")
-button.addEventListener("click", changeGrid)
+let button = document.querySelector("#change-grid");
+button.addEventListener("click", changeGrid);
