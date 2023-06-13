a61964b27e44cdc3131cb6149283bf5abf0dd2b8
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-13 13:15:30 +0800

    Complete game functionality with fixed grid

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let containerDiv = document.querySelector(".container")
+let containerDiv = document.querySelector(".container");
 
 for (let i = 1; i <= 256; i++) {
-    let div = document.createElement("div")
-    div.classList.add("inside-div")
-    containerDiv.appendChild(div)
+    let div = document.createElement("div");
+    div.classList.add("inside-div");
+    containerDiv.appendChild(div);
 }
+
+function changeColor() {
+    this.style.backgroundColor = "orange";
+}
+
+let grid = document.querySelectorAll(".inside-div");
+grid.forEach((square) => {
+    square.addEventListener("mouseover", changeColor);
+});
